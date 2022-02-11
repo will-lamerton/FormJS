d805021d266edc2b36a7debdac4e9c124af009a9
Will Lamerton <will@barr.media>
2022-02-11 14:12:04 +0000

    mod: finished FormJS class tests.

--- tests/jest/fileIndex.test.js
+++ tests/jest/fileIndex.test.js
@@ -21,7 +21,79 @@ test('You can create a new instance of FormJS', () => {
     expect(typeof formjs.error).toBe('function');
 });
 
-// validate test
-// version test
-// getInstances test
-// error test
+test('You can run an ad-hoc validation', async () => {
+    // Setup DOM
+    document.body.innerHTML = "<input id='test'></input>";
+
+    // Run validation that will fail:
+    formjs.validate('test', 'isEmail').catch((result) => {
+        resultToTest = result;
+    });
+
+    await new Promise(process.nextTick);
+
+    expect(typeof resultToTest).toBe('object');
+    expect(resultToTest).toMatchObject({
+        passed: false,
+        failed: 'Test must be a valid email!'
+    });
+
+    // Run validation that will pass:
+    document.getElementById('test').value = 'test@test.com';
+
+    formjs.validate('test', 'isEmail').then((result) => {
+        resultToTest = result;
+    });
+
+    await new Promise(process.nextTick);
+
+    expect(typeof resultToTest).toBe('object');
+    expect(resultToTest).toMatchObject({
+        passed: true,
+        failed: ''
+    });
+
+});
+
+test('You can get version', () => {
+    expect(formjs.version()).toBe(require('../../package.json').version);
+});
+
+test('You can get instances', () => {
+    // This should return an empty array as no forms have been made.
+    expect(formjs.getInstances()).toEqual(
+        expect.arrayContaining([])
+    );
+
+    expect(formjs.getInstances().length).toBe(0);
+
+    // Let's make a quick form...
+    document.body.innerHTML = '<form id="form"></form>';
+
+    formjs.create({
+        ref: 'form',
+        form: 'form',
+        onsubmit: {
+            method: '',
+            url: '',
+        }
+    });
+
+    expect(formjs.getInstances()).toEqual(
+        expect.arrayContaining([])
+    );
+
+    expect(formjs.getInstances().length).toBe(1);
+});
+
+test('FormJS can run `error` method', () => {
+    // Create spy.
+    const errorSpy = jest.spyOn(formjs, 'error');
+
+    // Run `create` setup to error.
+    formjs.create({});
+
+    // Error method should have been called.
+    expect(errorSpy).toHaveBeenCalled();
+
+});
